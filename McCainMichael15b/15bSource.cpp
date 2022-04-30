5cb61d8568ceada9438f1117ebacb46d8f5b4f9e
Michael McCain <michaelmc1989@gmail.com>
2022-04-30 14:20:38 -0400

    Add project files.

---
+++ McCainMichael15b/15bSource.cpp
@@ -0,0 +1,48 @@
+// Michael McCain
+// CIS 1202
+// 04/30/2022
+
+#include <iostream>
+#include <string>
+#include "15bSource.h"
+
+using namespace std;
+// prototypes
+int Half(int);
+
+float Half(float);
+
+double Half(double);
+
+int main()
+{
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout<< Half(a) << endl;
+	cout << Half(b) << endl;
+	cout << Half(c) << endl;
+
+
+
+	// end
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
+
+int Half(int num) 
+{
+
+}
+
+float Half(float num)
+{
+
+}
+
+double Half(double num)
+{
+
+}
